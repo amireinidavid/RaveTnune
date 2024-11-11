b6bdcdffa1dc9cb962f423831ae8ea71d1eb6853
amireinidavid <realheritagecompany@gmail.com>
2024-11-11 19:08:57 +0100

    first commit

---
+++ client/src/pages/admin/components/Header.tsx
@@ -0,0 +1,23 @@
+import { UserButton } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex items-center gap-3 mb-8">
+        <Link to="/" className="rounded-lg">
+          <img src="/ravetune.png" className="size-10 text-black" />
+        </Link>
+        <div>
+          <h1 className="text-3xl font-bold">
+            {" "}
+            welcome to RaveTnune Music Manager
+          </h1>
+          <p className="text-zinc-400 mt-1">Manage your music catalog</p>
+        </div>
+      </div>
+      <UserButton />
+    </div>
+  );
+};
+export default Header;
